92211bbf3c64bbc286ce30e3f56e3f6c6ebb89be
Larry1992Fei <1992lipengfei@gmail.com>
2026-05-22 12:37:01 +0800

    Configure PayDemo subpath deployment

--- frontend/src/config/payermaxDemoUrls.ts
+++ frontend/src/config/payermaxDemoUrls.ts
@@ -1,5 +1,6 @@
 const demoOrigin = typeof window !== 'undefined' ? window.location.origin : '';
+const demoBasePath = import.meta.env.BASE_URL.replace(/\/$/, '');
 
-export const DEMO_FRONT_CALLBACK_URL = `${demoOrigin}/callback`;
+export const DEMO_FRONT_CALLBACK_URL = `${demoOrigin}${demoBasePath}/callback`;
 export const DEMO_NOTIFY_URL = 'https://[your domain name]/[your notify URL]';
 export const DEMO_MIT_MANAGEMENT_URL = 'https://[your domain name]/[your subscription management URL]';
